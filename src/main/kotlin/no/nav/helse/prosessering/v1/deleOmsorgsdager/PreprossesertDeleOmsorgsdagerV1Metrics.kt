c79af6c96c8d507d221eaed340a3abbd4a7ebe40
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-09-08 14:55:01 +0200

    Legger til metrikk slik at vi kan se hvor mange sier ja på brukt dager og hvor mange dager legger de da inn

--- src/main/kotlin/no/nav/helse/prosessering/v1/deleOmsorgsdager/PreprossesertDeleOmsorgsdagerV1Metrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/deleOmsorgsdager/PreprossesertDeleOmsorgsdagerV1Metrics.kt
@@ -9,9 +9,21 @@ private val antallDeleOmsorgsdagerHistogram = Histogram.build()
     .help("Antall omsorgsdager man deler")
     .register()
 
+private val antallBruktedagerHistogram = Histogram.build()
+    .buckets(1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0)
+    .name("antall_brukte_dager_histogram")
+    .help("Antall omsorgsdager man allerede har brukt")
+    .register()
+
 private val jaNeiUtvidetRettCounter = Counter.build()
     .name("ja_nei_utvidet_rett_counter")
     .help("Teller for svar på ja/nei spørsmål om utvidet rett")
+    .labelNames("spm", "svar")
+    .register()
+
+private val jaNeiBrukteDagerCounter = Counter.build()
+    .name("ja_nei_brukte_dager_counter")
+    .help("Teller for svar på ja/nei spørsmål om brukte dager")
     .labelNames("spm", "svar")
     .register()
 
@@ -32,9 +44,13 @@ internal fun PreprossesertDeleOmsorgsdagerV1.reportMetrics() {
 
     jaNeiUtvidetRettCounter.labels("utvidetRett", harUtvidetRett.tilJaEllerNei()).inc()
 
+    fordelingSamboerEllerEktefelleCounter.labels("fordelingSamboerEllerEktefelle", overføreTilType.name).inc()
+
+    jaNeiBrukteDagerCounter.labels("brukteDager", harDeltDagerMedAndreTidligere.tilJaEllerNei()).inc()
+
     if(arbeidINorge && !borINorge) jobberINorgeMenBorIkkeINorgeCounter.labels("Jobber i Norge, men bor ikke i Norge", "Ja").inc()
 
-    fordelingSamboerEllerEktefelleCounter.labels("fordelingSamboerEllerEktefelle", overføreTilType.name).inc()
+    if(harDeltDagerMedAndreTidligere) antallBruktedagerHistogram.observe(antallDagerHarDeltMedAndre.toDouble())
 }
 
 private fun Boolean.tilJaEllerNei(): String = if (this) "Ja" else "Nei"
